859bd60ab59b797c4abb74ec0bf8084fd6f94540
Matthew <mahdi.mohaghegh2001@gmail.com>
2022-01-07 18:45:24 -0500

    changed the id from show-millionaire to show-millionaires

---
+++ script.js
@@ -0,0 +1,6 @@
+const main = document.getElementById("main");
+const main = document.getElementById("add-user");
+const main = document.getElementById("double");
+const main = document.getElementById("show-millionaires");
+const main = document.getElementById("sort");
+const main = document.getElementById("calculate-wealth");
