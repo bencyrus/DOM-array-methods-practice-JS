0b2e26a897d1fd3cfe0128d16c93babd1bdb7231
Matthew <mahdi.mohaghegh2001@gmail.com>
2022-01-07 18:54:22 -0500

    wrote functions for fetching random users and variablse to use data needed. Also created add to Data variable functionality

--- script.js
+++ script.js
@@ -1,6 +1,33 @@
 const main = document.getElementById("main");
-const main = document.getElementById("add-user");
-const main = document.getElementById("double");
-const main = document.getElementById("show-millionaires");
-const main = document.getElementById("sort");
-const main = document.getElementById("calculate-wealth");
+const addUserBtn = document.getElementById("add-user");
+const doubleBtn = document.getElementById("double");
+const showMillionaires = document.getElementById("show-millionaires");
+const sort = document.getElementById("sort");
+const calculateWealthBtn = document.getElementById("calculate-wealth");
+
+let data = [];
+
+getRandomUser();
+getRandomUser();
+getRandomUser();
+
+// fetch random user and add money
+async function getRandomUser() {
+  const res = await fetch("https://randomuser.me/api");
+  const data = await res.json();
+
+  const user = data.results[0];
+
+  const newUser = {
+    name: `${user.name.first} ${user.name.last}`,
+    money: Math.floor(Math.random() * 1000000),
+  };
+
+  addData(newUser);
+  console.log(newUser);
+}
+
+// Add new obj to data arr
+function addData(obj) {
+  data.push(obj);
+}
