d5fec43f0bab7898d1c2688e5b1dd252989d45d4
Matthew <mahdi.mohaghegh2001@gmail.com>
2022-01-07 19:47:44 -0500

    added filtering to create showing only millionaires

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const main = document.getElementById("main");
 const addUserBtn = document.getElementById("add-user");
 const doubleBtn = document.getElementById("double");
-const showMillionaires = document.getElementById("show-millionaires");
-const sort = document.getElementById("sort");
+const showMillionairesBtn = document.getElementById("show-millionaires");
+const sortBtn = document.getElementById("sort");
 const calculateWealthBtn = document.getElementById("calculate-wealth");
 
 let data = [];
@@ -42,6 +42,13 @@ function sortByRichest() {
   updateDOM();
 }
 
+// Filter only millionaires
+function showMillionaires() {
+  data = data.filter((user) => user.money > 1000000);
+
+  updateDOM();
+}
+
 // Add new obj to data arr
 function addData(obj) {
   data.push(obj);
@@ -72,4 +79,5 @@ function formatMoney(number) {
 // Event listeners
 addUserBtn.addEventListener("click", getRandomUser);
 doubleBtn.addEventListener("click", doubleMoney);
-sort.addEventListener("click", sortByRichest);
+sortBtn.addEventListener("click", sortByRichest);
+showMillionairesBtn.addEventListener("click", showMillionaires);
